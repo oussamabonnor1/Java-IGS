07057125898acc5b22569e276374a95af676ef49
Oussama BONNOR <oussamabonnor@gmail.com>
2017-09-03 00:37:29 +0100

    adding directory

---
+++ src/semaine3/Chat.java
@@ -0,0 +1,28 @@
+package semaine3;
+
+class Chat {
+       
+	 	String eyes;
+	 	String color;
+	    String name;
+	    final int age = 0;
+	    boolean sovageOuDomestique;
+	    String voix =  "miaw";
+
+	public Chat(String eyes, String color, String name) {
+		this.eyes = eyes;
+		this.color = color;
+		this.name = name;
+	}
+
+	public void miao(){
+	    	System.out.println("Miawwwww..");
+	    }
+	    public void myInfo(){
+	    	System.out.println("Miaww my name is "+name);
+	    }	
+	    public int apresUnNom(){
+	    	return age+1;
+	    }
+	    
+}
---
+++ src/semaine3/Chien.java
@@ -0,0 +1,10 @@
+package semaine3;
+
+public class Chien {
+	String eyes;
+ 	String color;
+    String name;
+    int age;
+    boolean sovageOuDomestique;
+    String voix = "haw";
+}
